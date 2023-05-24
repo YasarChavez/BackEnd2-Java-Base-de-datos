ab7ce7a96bcc2d93d98c9ea503e77c6cd569f91e
YasarChavez <tneniitoo@gmail.com>
2023-05-23 21:58:35 -0300

    Guia 11 / Reformat Code

--- Guia11ExEjerc04/src/Ejerc04.java
+++ Guia11ExEjerc04/src/Ejerc04.java
@@ -4,6 +4,5 @@ public class Ejerc04 {
     public static void main(String[] args) {
         SimuladorService simuladorService = new SimuladorService();
         simuladorService.menu();
-
     }
 }
