fc0811240dd2fedafd87db702bd950219b9c477b
YasarChavez <tneniitoo@gmail.com>
2023-05-29 20:15:15 -0300

    Guia 12 / Ejerc04 UPDATES

--- Guia12Ejerc01/src/Ejerc01.java
+++ Guia12Ejerc01/src/Ejerc01.java
@@ -11,15 +11,15 @@ public class Ejerc01 {
         AnimalesService animalesService = new AnimalesService();
         ArrayList<Animal> animales = new ArrayList<>();
 
-        Perro perroObj = new Perro("Firulais", "Carne", 7, "Pitbull");
+        Animal perroObj = new Perro("Firulais", "Carne", 7, "Pitbull");
         animales.add(perroObj);
         perroObj.alimentarse();
 
-        Gato gatoObj = new Gato("Pelusa", "Croquetas", 5, "Siames");
+        Animal gatoObj = new Gato("Pelusa", "Croquetas", 5, "Siames");
         animales.add(gatoObj);
         gatoObj.alimentarse();
 
-        Caballo caballoObj = new Caballo("Pegaso", "Alfalfa", 10, "Caballo andaluz");
+        Animal caballoObj = new Caballo("Pegaso", "Alfalfa", 10, "Caballo andaluz");
         animales.add(caballoObj);
         caballoObj.alimentarse();
 
