11218116d958028f36bf9b8841de115825fd8c0c
YasarChavez <tneniitoo@gmail.com>
2023-06-10 17:17:35 -0300

    Guia 13 JARVIS FALTA TERMINAR /UPDATES

--- JARVIS/src/Entidad/Consola.java
+++ JARVIS/src/Entidad/Consola.java
@@ -1,4 +1,27 @@
 package Entidad;
 
 public class Consola {
+    private float consumo = 1;
+
+    public Consola() {
+    }
+
+    public Consola(float consumo) {
+        this.consumo = consumo;
+    }
+
+    public float getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(float consumo) {
+        this.consumo = consumo;
+    }
+
+    @Override
+    public String toString() {
+        return "Consola{" +
+                "consumo=" + consumo +
+                '}';
+    }
 }
--- JARVIS/src/Entidad/Sintetizador.java
+++ JARVIS/src/Entidad/Sintetizador.java
@@ -1,4 +1,27 @@
 package Entidad;
 
 public class Sintetizador {
+    private  float consumo = 1;
+
+    public Sintetizador() {
+    }
+
+    public Sintetizador(float consumo) {
+        this.consumo = consumo;
+    }
+
+    public float getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(float consumo) {
+        this.consumo = consumo;
+    }
+
+    @Override
+    public String toString() {
+        return "Sintetizador{" +
+                "consumo=" + consumo +
+                '}';
+    }
 }
